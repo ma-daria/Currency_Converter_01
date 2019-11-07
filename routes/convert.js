0109b21716a0e3507d346e050d694dad3df6134f
ma-daria <Can't finish GitHub sharing process 				Successfully created project 'test_ITOB' on GitHub, but initial commit failed: 				*** Please tell me who you are. 				 				Run 				 				git config --global user.email "you@example.com" 				git config --global user.name "Your Name" 				 				to set your account's default identity. 				Omit --global to set the identity only in this repository. 				 				unable to auto-detect email address (got 'ma-daria@madaria-UX32LA.(none)')>
2019-11-07 11:27:02 +0300

    add convert

---
+++ routes/convert.js
@@ -0,0 +1,60 @@
+let express = require('express');
+let router = express.Router();
+const axios = require('axios');
+
+router.get('/', async function(req, res, next) {
+    let moneyFrom = req.query.moneyFrom;
+    let currencyFrom = req.query.currencyFrom;
+    let currencyTo = req.query.currencyTo;
+    let currency = await axios.get('https://www.cbr-xml-daily.ru/daily_json.js');
+    let mFrom;
+    let mTo;
+
+    if (currencyFrom === 'RUS'){
+        mFrom = 1;
+    }else{
+        mFrom = currency.data.Valute[currencyFrom].Value;
+    }
+    if (currencyTo === 'RUS'){
+        mTo = 1;
+    }else {
+        mTo = currency.data.Valute[currencyTo].Value;
+    }
+
+    let moneyTo = (moneyFrom * mFrom) / mTo;
+
+
+    let currencyList = Object.keys(currency.data.Valute);
+    currencyList.push('RUS');
+
+    console.log(typeof(currencyList));
+
+    let currencyListFrom = Object.create(currencyList);
+    let currencyListTo = Object.create(currencyList);
+
+    ToUnshift(currencyListFrom, currencyFrom);
+    ToUnshift(currencyListTo, currencyTo);
+
+
+
+    res.render('index', {
+        mFrom: moneyFrom,
+        mTo: moneyTo.toFixed(2),
+        currencyFrom: currencyListFrom,
+        currencyTo: currencyListTo
+    });
+});
+
+
+
+
+function  ToUnshift(list, currency){
+    let i = list.indexOf(currency);
+    list.splice(i, 1);
+    list.unshift(currency);
+}
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@ let express = require('express');
 let router = express.Router();
 const axios = require('axios');
 
-/* GET home page. */
 router.get('/', async function(req, res, next) {
   let  currency = await axios.get('https://www.cbr-xml-daily.ru/daily_json.js');
-  let currency_list = Object.keys(currency.data.Valute);
-  currency_list.push('RUS');
+
+
+
+  let currencyList = Object.keys(currency.data.Valute);
+  currencyList.push('RUS');
 
 
   res.render('index', {
     mFrom: null,
     mTo: null,
-    currency: currency_list
+    currencyFrom: currencyList,
+    currencyTo: currencyList
   });
 });
 
